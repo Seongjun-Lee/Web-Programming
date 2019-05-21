f99576c47dbad48dfc7fde114046e8568c7f9e6e
Seongjun-Lee <yuhancam1@naver.com>
2019-05-21 17:41:15 +0900

    재정리

--- Web-Programming/Project/Register/RegisterSQL.sql
+++
@@ -1,9 +0,0 @@
-create table User(
-num int not null auto_increment,
-id text not null,
-password text not null,
-email text not null,
-nickname text not null,
-phone text not null,
-primary key(num)
-)
--- Web-Programming/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: yuhan
- * Date: 2019-05-03
- * Time: 오후 6:32
- */
-
-echo (hello);
